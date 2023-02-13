1049ca2ad414f931b2083e606f6d04903d94c219
IrinaChoulgan <irina_choulgan@icloud.com>
2023-02-13 15:30:25 +0100

    created the application architecture

---
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import style from './Skills.module.css';
+
+const Skills = () => {
+  return <div></div>;
+};
+
+export default Skills;
---
+++ frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import style from './Testimonial.module.css';
+
+const Testimonial = () => {
+  return <div>Testimonial</div>;
+};
+
+export default Testimonial;
